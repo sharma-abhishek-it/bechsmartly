e8e4349c41683d1eb52a7a6400bf20bd9b70dfab
Abhishek Sharma <sharma.abhishek.it@gmail.com>
2014-06-21 13:05:51 +0530

    Moving to new design

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,11 @@
 //= require_tree .
+
+$('.dialog-container').waypoint(function(direction){
+  if(direction === 'down') {
+    $(this).addClass('fixed');
+  } else {
+    $(this).removeClass('fixed');
+  }
+
+
+},{offset: 100});
